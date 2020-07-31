da76ee5e7ac0a82d7eed72aec2b91a88d1ab0888
Sahil Sharma <sahilaug@gmail.com>
2020-07-31 14:00:01 +0530

    try download module

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Hello !!")
+
+var download = require('download-file')
+ 
+var url = "https://github.com/qontract/qontract/releases/download/0.13.1/qontract.jar"
+ 
+var options = {
+    directory: "./qontract/lib/test",
+    filename: "qontract.jar"
+}
+ 
+download(url, options, function(err){
+    if (err) throw err
+    console.log("meow")
+})
