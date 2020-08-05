09c764c4e26edae5d8a82522c7ba3627b4e0bda4
Sahil Sharma <sahilaug@gmail.com>
2020-08-05 10:08:12 +0530

    adds another method

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-const fs = require('fs');
-const download = require('download');
+const init = () => {
+    const fs = require('fs');
+    const download = require('download');
 
-(async () => {
-    await download('https://github.com/qontract/qontract/releases/download/0.13.1/qontract.jar', 'dist');
-})();
+    (async () => {
+        await download('https://github.com/qontract/qontract/releases/download/0.13.1/qontract.jar', 'qontract/lib');
+    })();
+}
+
+init();
+
+
+const startQontractStubServer = () => {
+    const execSh = require('exec-sh');
+    const path = require('path');
+
+    const jarPath = path.resolve('qontract/lib/qontract.jar');
+    const contractsPath = path.resolve('qontract/contracts/*.qontract');
+
+    execSh(
+        `java -jar ${jarPath} stub ${contractsPath}`,
+        { cwd: 'qontract' },
+        err => {
+          if (err) {
+            console.log('Exit code: ', err.code);
+          }
+        }
+      );
+}
+
+exports.startQontractStubServer = startQontractStubServer;
