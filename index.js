d8105c2118d86f7e27f96695f7e29cbaa9ae7067
Sahil Sharma <sahilaug@gmail.com>
2020-08-05 09:48:37 +0530

    try download on post install

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-module.exports = () => {
-    const fs = require('fs');
-    const download = require('download');
-    console.log("qontract jar download starting")
-    
-    (async () => {
-        await download('https://github.com/qontract/qontract/releases/download/0.13.1/qontract.jar', 'dist');
-        console.log("qontract jar download complete")
-    })();
+const fs = require('fs');
+const download = require('download');
 
-}
+(async () => {
+    await download('https://github.com/qontract/qontract/releases/download/0.13.1/qontract.jar', 'dist');
+})();
